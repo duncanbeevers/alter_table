4f3edc4535eac6536908a97afe5542e5dec85e15
Duncan Beevers <duncan@dweebd.com>
2010-06-28 14:56:21 -0700

    Add query-counting helper code from Rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,17 @@ require 'ruby-debug'
 ActiveRecord::Base.configurations = YAML::load(File.open(File.join(TEST_ROOT, 'database.yml')))
 ActiveRecord::Base.establish_connection(:alter_table_test)
 
+# Cribbed from Rails tests
+ActiveRecord::Base.connection.class.class_eval do
+  IGNORED_SQL = [/^PRAGMA/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /SHOW FIELDS/]
+  
+  def execute_with_query_record(sql, name = nil, &block)
+    $queries_executed ||= []
+    $queries_executed << sql unless IGNORED_SQL.any? { |r| sql =~ r }
+    execute_without_query_record(sql, name, &block)
+  end
+  
+  alias_method_chain :execute, :query_record
+end
+
 require File.join(TEST_ROOT, '../init')
