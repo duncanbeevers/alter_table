573b9f0431ec6a87303348aad5cb31d33164c6f3
Duncan Beevers <duncan@dweebd.com>
2010-06-28 17:09:01 -0700

    Preserve operation order when altering table

--- lib/alter_table.rb
+++ lib/alter_table.rb
@@ -9,123 +9,116 @@ module AlterTable
   
   module InstanceMethods
     def alter_table table_name
-      acc = TableOperationAccumulator.new(table_name)
+      acc = TableOperationAccumulator.new(table_name, self)
       yield acc
       
       report_operations(acc) if ActiveRecord::Migration.verbose
-      execute("ALTER TABLE #{quote_table_name(table_name)} #{sql_from_accumulator(acc)}")
+      execute("ALTER TABLE %s %s" % [
+        quote_table_name(table_name),
+        acc.sql
+      ])
     end
     
     def report_operations(acc)
-      if !acc.add_columns.blank?
-        puts "* Adding columns:\n%s" %
-          acc.add_columns.map { |ac| "\t%s" % ac.map { |c| c.inspect } }.join("\n")
-      end
-      if !acc.remove_columns.blank?
-        puts "* Removing columns: %s" %
-          acc.remove_columns.map { |rc| "\t%s" % rc.inspect }.join("\n")
-      end
-    end
-    
-    def sql_from_accumulator acc
-      [ sql_from_accumulator_for_add_columns(acc),
-        sql_from_accumulator_for_remove_columns(acc),
-        sql_from_accumulator_for_add_indexes(acc),
-        sql_from_accumulator_for_remove_indexes(acc),
-        sql_from_accumulator_for_rename_columns(acc)
-      ].select { |s| !s.blank? }.join(',')
-    end
-    
-    def sql_from_accumulator_for_add_columns acc
-      acc.add_columns.map { |args|
-        column_name = args.shift
-        type        = args.shift
-        options     = args.extract_options!
-        
-        sql = "ADD #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
-        
-        add_column_options!(sql, options)
-        sql
-      }.join(',')
-    end
-    
-    def sql_from_accumulator_for_remove_columns acc
-      acc.remove_columns.map { |column_name|
-        "DROP #{quote_column_name(column_name)}"
-      }.join(',')
-    end
-    
-    def sql_from_accumulator_for_add_indexes acc
-      acc.add_indexes.map { |column_names, options|
-        index_type = options[:unique] ? 'UNIQUE' : ''
-        index_name = options[:name] || index_name(acc.table_name, :column => column_names)
-        quoted_column_names = column_names.map { |c| quote_column_name(c) }.join(',')
-        "ADD %s INDEX %s (%s)" % [
-          index_type,
-          quote_column_name(index_name),
-          quoted_column_names
-        ]
-      }.join(',')
-    end
-    
-    def sql_from_accumulator_for_remove_indexes acc
-      acc.remove_indexes.map { |index_name|
-        "DROP INDEX %s" % [ quote_column_name(index_name) ]
-      }
-    end
-    
-    def sql_from_accumulator_for_rename_columns acc
-      return nil if acc.rename_columns.blank?
-      
-      col_defs = select("SHOW COLUMNS FROM #{quote_table_name(acc.table_name)}").inject({}) do |a, c|
-        a[c['Field']] = c['Type']
-        a
-      end
-      
-      acc.rename_columns.map { |old_name, new_name|
-        current_type = 
-        "CHANGE %s %s %s" % [
-          quote_column_name(old_name),
-          quote_column_name(new_name),
-          col_defs[old_name]
-        ]
-      }
+      # if !acc.add_columns.blank?
+      #   puts "* Adding columns:\n%s" %
+      #     acc.add_columns.map { |ac| "\t%s" % ac.map { |c| c.inspect } }.join("\n")
+      # end
+      # if !acc.remove_columns.blank?
+      #   puts "* Removing columns: %s" %
+      #     acc.remove_columns.map { |rc| "\t%s" % rc.inspect }.join("\n")
+      # end
     end
   end
   
   class TableOperationAccumulator
-    attr_reader :table_name,
-                :add_columns, :remove_columns, :rename_columns,
-                :add_indexes, :remove_indexes
-    
-    def initialize(table_name)
-      @table_name     = table_name
-      @add_columns    = []
-      @remove_columns = []
-      @rename_columns = []
-      @add_indexes    = []
-      @remove_indexes = []
+    def initialize(table_name, base)
+      @table_name = table_name
+      @base       = base
+      @operations = []
     end
     
     def add_column *args
-      @add_columns << args
+      @operations << [ :add_column, args ]
     end
     
     def remove_column *column_names
-      @remove_columns += column_names.flatten
+      @operations << [ :remove_column, column_names.flatten ]
     end
     alias :remove_columns :remove_column
     
     def rename_column old_name, new_name
-      @rename_columns << [ old_name, new_name ]
+      @operations << [ :rename_column, [ old_name, new_name ] ]
     end
     
     def add_index column_names, options = {}
-      @add_indexes << [ Array(column_names), options ]
+      @operations << [ :add_index, [ Array(column_names), options ] ]
     end
     
     def remove_index index_name
-      @remove_indexes << index_name
+      @operations << [ :remove_index, [ index_name ] ]
+    end
+    
+    def sql
+      @operations.map { |operation| sql_for_operation(*operation) }.
+        select { |s| !s.blank? }.join(',')
+    end
+    
+    private
+    def sql_for_operation(operation, payload)
+      case operation
+      when :add_column    then sql_for_add_column(*payload)
+      when :remove_column then sql_for_remove_column(*payload)
+      when :rename_column then sql_for_rename_column(*payload)
+      when :add_index     then sql_for_add_index(*payload)
+      when :remove_index  then sql_for_remove_index(*payload)
+      end
+    end
+    
+    def sql_for_add_column(*args)
+      column_name = args.shift
+      type        = args.shift
+      options     = args.extract_options!
+      
+      sql = "ADD %s %s" % [
+        @base.quote_column_name(column_name),
+        @base.type_to_sql(type, options[:limit], options[:precision], options[:scale])
+      ]
+      
+      @base.add_column_options!(sql, options)
+      sql
+    end
+    
+    def sql_for_remove_column(column_name)
+      "DROP %s" % @base.quote_column_name(column_name)
+    end
+    
+    def sql_for_rename_column(old_name, new_name)
+      @col_defs ||= @base.send(:select, "SHOW COLUMNS FROM %s" % @base.quote_table_name(@table_name)).inject({}) do |a, c|
+        a[c['Field']] = c['Type']
+        a
+      end
+      
+      "CHANGE %s %s %s" % [
+        @base.quote_column_name(old_name),
+        @base.quote_column_name(new_name),
+        @col_defs[old_name]
+      ]
+    end
+    
+    def sql_for_add_index(column_names, options)
+      index_type = options[:unique] ? 'UNIQUE' : ''
+      index_name = options[:name] || @base.index_name(@table_name, :column => column_names)
+      quoted_column_names = column_names.map { |c| @base.quote_column_name(c) }.join(',')
+      "ADD %s INDEX %s (%s)" % [
+        index_type,
+        @base.quote_column_name(index_name),
+        quoted_column_names
+      ]
+    end
+    
+    def sql_for_remove_index(index_name)
+      "DROP INDEX %s" % [ @base.quote_column_name(index_name) ]
     end
   end
   
